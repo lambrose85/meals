b0da32f4ab2afad87de5d3c3d149bf4f01478a17
Lucas Ambrose <lucasambrose@siu.edu>
2025-06-28 12:57:34 -0500

    edited main.go to include struct pulled from json

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+	"encoding/json"
+)
+
+type Meal struct{
+	Name string `json:"name"`
+	Day string `json:"day"`
+	Ingredients []Ingredient `json:"ingredients"`
+}
+type Ingredient struct{
+	Name string `json:"name"`
+	Amount string `json:"amount"`
+}
+
+func main(){
+
+
+
+
+}
+
+
+
